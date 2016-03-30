f400bd4886fd51974c5bb6e2b60f604de78b9829
Naveen512 <bommidinaveen@gmail.com>
2016-03-30 19:46:18 +0530

    First Commit

---
+++ TaskExamples/TaskExa_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExamples
+{
+    public class TaskExa_1
+    {
+        static void Main(string[] args)
+        {
+
+            Task t= Task.Run(
+                  () =>
+                  {
+
+                      int i = 0;
+                      for (i = 0; i < 1000; i++)
+                      {
+
+                      }
+                      Console.WriteLine(" Total i count is {0} ", i);
+
+                  }
+                );
+            Console.WriteLine(" After task Before t.wait() ");
+            t.Wait();
+
+            Console.WriteLine(" After task after t.wait() ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TaskExamples/TaskExam_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExamples
+{
+    public class TaskExam_2
+    {
+        static void Main()
+        {
+            Task t = Task.Factory.StartNew(
+                  () =>
+                  {
+
+                      int count = 0;
+                      for (count = 0; count < 1000; count++)
+                      {
+
+                      }
+                      Console.Write(" Total count = {0} ", count);
+
+                  }
+                );
+
+            Console.WriteLine(" Before t.wait() ");
+            Console.WriteLine(" Before t.wait()2 ");
+            Console.WriteLine(" Before t.wait()3 ");
+            Console.WriteLine(" Before t.wait()4 ");
+            Console.WriteLine(" Before t.wait()5 ");
+            Console.WriteLine(" Before t.wait()6 ");
+
+            t.Wait();
+
+            Console.Write(" After t.wait() ");
+            Console.ReadKey();
+        }
+    }
+}
